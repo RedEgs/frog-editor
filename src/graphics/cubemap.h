8945ab9ddf9806860e85212e50426126ad8d725c
Charlie Gough <tothemuun21@gmail.com>
2026-02-03 18:04:56 +0000

    Added Cubemaps support along with a sky component, so far it only support cubemaps but will soon support colouring, lighting etc. More cleaning up around the codebase as I improve.

--- src/graphics/cubemap.h
+++ src/graphics/cubemap.h
@@ -11,10 +11,38 @@
 #include "graphics/shader.h"
 
 class Cubemap {
+private:
+    const char *frag_shader =
+        "#version 330 core"
+        "layout (location = 0) in vec3 aPos;"
+
+        "out vec3 TexCoords;"
+
+        "uniform mat4 projection;"
+        "uniform mat4 view;"
+
+        "void main()"
+        "{"
+        "    TexCoords = aPos;"
+        "    gl_Position = projection * view * vec4(aPos, 1.0);"
+        "}";
+
+    const char *vert_shader =
+        "#version 330 core"
+        "out vec4 FragColor;"
+        "in vec3 TexCoords;"
+        "uniform samplerCube skybox;"
+        "void main()"
+        "{"
+        "   FragColor = texture(skybox, TexCoords);"
+        "}";
+
 public:
     unsigned int textureID = 0;
     unsigned int VAO = 0;
     unsigned int VBO = 0;
+
+
 
     Cubemap(
         const char* px,
